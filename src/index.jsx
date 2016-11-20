87a4e0fbe86a4874f1c4bd4fc76e2d1bf590e416
Brandon Horst <brandonhorst@gmail.com>
2016-11-20 13:08:50 -0500

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 /** @jsx createElement */
 import _ from 'lodash'
 import { createElement } from 'elliptical'
-// import { showNotification } from 'lacona-api'
-import { URL } from 'lacona-phrases'
-import { onActivate } from 'lacona-source-helpers'
+import { showNotification } from 'lacona-api'
+import { URL, Command, String } from 'lacona-phrases'
+import { everyInterval } from 'lacona-source-helpers'
 import { join } from 'path'
 import parseLinkHeader from 'parse-link-header'
 import fetch from 'node-fetch'
 
 const API_URL = 'https://api.github.com'
 const FETCH_REPOS_URL = '/user/repos'
+const FETCH_ORGS_URL = '/user/orgs'
 const STANDARD_QS = '?per_page=100'
-const GITHUB_IMG = join(__dirname, '../img/GitHub Filled-50.png')
+const GITHUB_IMG = join(__dirname, '../icon.png')
 
 async function fetchAllPages (accessToken, url, mapResults) {
   const results = []
@@ -22,11 +23,10 @@ async function fetchAllPages (accessToken, url, mapResults) {
   while (nextURL) {
     const res = await fetch(nextURL, {headers: {Authorization: `token ${accessToken}`}})
     if (!res.ok) {
-      console.log('ERROR ROIERJ')
       throw new Error(`${nextURL} call returned status ${res.status} : ${res.statusText}`)
     }
     const linkHeader = parseLinkHeader(res.headers.get('Link'))
-    nextURL = linkHeader.next ? linkHeader.next.url : null
+    nextURL = (linkHeader && linkHeader.next) ? linkHeader.next.url : null
     const body = await res.json()
 
     const newResults = _.map(body, mapResults)
@@ -39,24 +39,107 @@ async function fetchAllPages (accessToken, url, mapResults) {
 
 async function fetchGithubRepos ({props}) {
   if (props.accessToken) {
-    const results = await fetchAllPages(props.accessToken, `${API_URL}${FETCH_REPOS_URL}${STANDARD_QS}`, repo => ({
-      name: repo.full_name,
-      url: repo.html_url,
-      id: repo.id
-    }))
+    try {
+      const results = await fetchAllPages(props.accessToken, `${API_URL}${FETCH_REPOS_URL}${STANDARD_QS}`, repo => ({
+        name: repo.full_name,
+        url: repo.html_url,
+        id: repo.id
+      }))
 
-    return results
+      return results
+    } catch (e) {
+      console.error('Github Repo Fetch Error: ', e)
+      return []
+    }
   }
   return []
 }
 
-const RepoSource = onActivate(fetchGithubRepos)
+async function fetchGithubOrganizations ({props}) {
+  if (props.accessToken) {
+    try {
+      const results = await fetchAllPages(props.accessToken, `${API_URL}${FETCH_ORGS_URL}${STANDARD_QS}`, org => ({
+        name: org.login,
+        url: `https://github.com/${encodeURIComponent(org.login)}`,
+        id: org.id
+      }))
+
+      return results
+    } catch (e) {
+      console.error('Github Repo Fetch Error: ', e)
+      return []
+    }
+  }
+  return []
+}
+
+// Look for new repos every hour
+const RepoSource = everyInterval(fetchGithubRepos, 60 * 60 * 1000, [])
+const OrgSource = everyInterval(fetchGithubOrganizations, 60 * 60 * 1000, [])
+
+const NewRepoName = {
+  mapResult (result) {
+    if (_.includes(result, '/')) {
+      const parts = result.match(/.*\/.*/)
+      return {repo: parts[1], org: parts[0]}
+    } else {
+      return {repo: result}
+    }
+  },
+  describe () {
+    return (
+      <placeholder argument='GitHub repository'>
+        <String splitOn={/\s/} filter={input => !/\s/.test(input)} />
+      </placeholder>
+    )
+  }
+}
+
+const CreateRepoCommand = {
+  extends: [Command],
+  execute (result) {
+    openURL({url: 'https://github.com/new'})
+  },
+  describe ({observe, config}) {
+    // const accessToken = config.github.authentication
+    //   ? config.github.authentication.access_token
+    //   : null
+
+    // const organizations = observe(<OrgSource accessToken={accessToken} />)
+    // console.log(organizations)
+    // const orgItems = _.map(organizations, org => ({
+    //   text: org.name,
+    //   value: org.name
+    // }))
+
+    // return (
+    //   <sequence unique>
+    //     <literal text='create ' />
+    //     <list items={['repository ', 'repo ']} optional limited limit={1} />
+    //     <NewRepoName merge ellipsis />
+    //     {orgItems.length ? (
+    //       <sequence id='org'>
+    //         <literal text=' in ' />
+    //         <list items={['organization ', 'org ']} optional limited limit={1} />
+    //         <placeholder argument='organization' suppressEmpty={false} merge>
+    //           <list items={orgItems} />
+    //         </placeholder>
+    //       </sequence>
+    //     ) : null}
+    //   </sequence>
+    // )
+    return <list
+      items={['create new GitHub repository', 'create GitHub repository', 'create new repository', 'create repository']}
+      qualifier='GitHub'
+      limit={1} />
+  }
+}
 
 const RepoURL = {
   extends: [URL],
   describe ({observe, config}) {
-    const accessToken = config.github.authentication
-      ? config.github.authentication.access_token
+    const accessToken = config.authentication
+      ? config.authentication.access_token
       : null
 
     const repos = observe(<RepoSource accessToken={accessToken} />)
@@ -75,18 +158,4 @@ const RepoURL = {
   }
 }
 
-export const extensions = [RepoURL]
-
-export const actions = {
-  githubAuth (schema) {
-    return new Promise((resolve, reject) => {
-      if (currentServer) {
-        currentServer.close(() => {
-          startServer(resolve, reject)
-        })
-      } else {
-        startServer(resolve, reject)
-      }
-    })
-  }
-}
+export const extensions = [RepoURL, CreateRepoCommand]
